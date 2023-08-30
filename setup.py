e99d42f25163ddeb541f695b3c176a4acff0d72a
Kuschel <fkuschel@gmx.de>
2023-08-30 13:47:20 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='energysys_components',
+    version='1.0',
+    packages=['various'],
+    url='https://github.com/ZBT-Tools/energysys_components',
+    license='',
+    author='Kuschel',
+    author_email='',
+    description=''
+)
